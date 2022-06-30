d380f0c2cdff605600f93ec68d99fc562780361b
sihyung <buzzcatfish@gmail.com>
2022-07-01 01:31:41 +0900

    modal 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import Modal from './components/Modal';
 
 function App() {
+    const [isActived, setIsActived] = useState(false);
+
+    const handleModal = status => {
+        setIsActived(status);
+    };
+
     return (
         <div className="App">
-            <Modal />
-            <button type="button">open modal</button>
+            <Modal isActived={isActived} handleModal={handleModal} />
+            <button type="button" onClick={() => handleModal(true)}>
+                open modal
+            </button>
         </div>
     );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,15 +13,36 @@ const Root = styled.div`
     background-color: rgba(0, 0, 0, 0.5);
 `;
 const Container = styled.div`
+    position: relative;
     width: 400px;
     height: 300px;
+    padding: 10px;
+    box-sizing: border-box;
     background-color: #fff;
 `;
+const Closer = styled.button`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+`;
 
-export default () => {
+export default props => {
+    const { isActived, handleModal } = props;
+
     return (
-        <Root>
-            <Container>Modal Text</Container>
-        </Root>
+        <>
+            {isActived ? (
+                <Root>
+                    <Container>
+                        Modal Text
+                        <Closer onClick={() => handleModal(false)}>
+                            close
+                        </Closer>
+                    </Container>
+                </Root>
+            ) : (
+                ''
+            )}
+        </>
     );
 };
